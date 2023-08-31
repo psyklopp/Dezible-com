ce314c5496b5b5851df27d158b3b0b2d3f826853
psyklopp <d3zible@gmail.com>
2023-08-31 19:47:41 +0200

    adding hugo theme

---
+++ content/basics/first-content.md
@@ -0,0 +1,6 @@
+---
+title: "First Content"
+date: 2023-08-31T19:34:24+02:00
+draft: true
+---
+
---
+++ docs/index.search.js
@@ -0,0 +1,30 @@
+var relearn_search_index = [
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Basics",
+    "uri": "/basics/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "My New Hugo Site",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
