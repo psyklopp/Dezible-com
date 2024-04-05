42c63e425d323227576442322d405b481e048713
psyklopp <d3zible@gmail.com>
2024-04-05 21:44:48 +0200

    Added Linux to basics

---
+++ content/basics/third-content/_index.md
@@ -0,0 +1,12 @@
+---
+title: "Linux"
+date: 2024-04-04T19:34:24+02:00
+draft: false
+weight: 3
+---
+
+**Ques**: Why is globbing not working with asterisk in Kali Linux?
+
+**Ans**: {{< figure src="https://raw.githubusercontent.com/psyklopp/AdityaBhardwaj-dev/main/images/Linux-x001.jpg" title="Using setopt nullglob" >}}
+
+
--- docs/index.search.js
+++ docs/index.search.js
@@ -28,6 +28,13 @@ var relearn_search_index = [
     "uri": "/digital-signatures/index.html"
   },
   {
+    "content": "Ques: Why is globbing not working with asterisk in Kali Linux?\nAns: Using setopt nullglob ",
+    "description": "",
+    "tags": null,
+    "title": "Linux",
+    "uri": "/basics/third-content/index.html"
+  },
+  {
     "content": "Blind signatures are an extension of digital signatures, which allows a user to obtain a signature on a message without revealing the contents of the message to the signer.\nA good analogy to blind signatures is anonymous voting. A voter who wants to cast a valid vote in an envelope, wants a signature from the official election authority. The officials can verify the credentials of the voters and sign the envelope containing the vote, without learning the contents. This ensures the integrity of the vote, and authorities learn nothing about the voters’ selection.\nThe idea of blind signatures was introduced by David Chaum in his paper 1 which was based on the RSA signature scheme.\nD. L. Chaum, Blind Signatures for Untraceable Payments, Jan. 1983, doi: 10.1007/978-1-4757-0602-4_18, https://bitcoin-class.org/readings/Chaum-blind-signatures.PDF ↩︎\n",
     "description": "",
     "tags": null,
